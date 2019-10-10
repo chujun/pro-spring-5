9b5feab2115f419b04666e9f37fb8981b62df38b
jun.chu <1571970439@qq.com>
2019-10-10 17:42:58 +0800

    冲突ConstructorConfusion 相同

--- chapter03/constructor-injection/src/main/java/com/apress/prospring5/ch3/xml/ConstructorConfusion.java
+++ chapter03/constructor-injection/src/main/java/com/apress/prospring5/ch3/xml/ConstructorConfusion.java
@@ -4,28 +4,40 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class ConstructorConfusion {
 
-	private String someValue;
+    private String someValue;
 
-	public ConstructorConfusion(String someValue) {
-		System.out.println("ConstructorConfusion(String) called");
-		this.someValue = someValue;
-	}
+    public ConstructorConfusion(String someValue) {
+        System.out.println("ConstructorConfusion(String) called");
+        this.someValue = someValue;
+    }
 
-	public ConstructorConfusion(int someValue) {
-		System.out.println("ConstructorConfusion(int) called");
-		this.someValue = "Number: " + Integer.toString(someValue);
-	}
+    public ConstructorConfusion(int someValue) {
+        System.out.println("ConstructorConfusion(int) called");
+        this.someValue = "Number: " + Integer.toString(someValue);
+    }
 
-	public static void main(String... args) {
-		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-		ctx.load("classpath:spring/app-context-xml.xml");
-		ctx.refresh();
-		ConstructorConfusion cc = (ConstructorConfusion) ctx.getBean("constructorConfusion");
-		System.out.println(cc);
-		ctx.close();
-	}
+    public static void main(String... args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/app-context-xml.xml");
+        ctx.refresh();
+        ConstructorConfusion cc = (ConstructorConfusion) ctx.getBean("constructorConfusion");
+        System.out.println(cc);
+        ctx.close();
+    }
 
-	public String toString() {
-		return someValue;
-	}
+	/*
+	<bean id="constructorConfusion"
+        class="com.apress.prospring5.ch3.xml.ConstructorConfusion">
+        <constructor-arg type="int">
+            <value>90</value>
+        </constructor-arg>
+    </bean>
+    去掉 type="int"属性，则会
+    "ConstructorConfusion(String) called"
+    被调用
+	 */
+
+    public String toString() {
+        return someValue;
+    }
 }
